4341ce496df76c5bb10a10a60c922617c1798a35
sharpobject <sharpobject@13e503dc-87b5-11de-828c-4586019eda89>
2009-08-27 12:09:16 +0000

    Added fun constants and used some of them.

--- util.h
+++ util.h
@@ -5,5 +5,10 @@
 const float PI=(3.141592653589793238462643383279502f);
 const float TEXTURE_SZ =(256.0f);
 const float DEGREE=PI/180;
+const float FULL_CIRCLE=PI*2;
+const float FACING_RIGHT=0;
+const float FACING_UP=PI*.5f;
+const float FACING_LEFT=PI;
+const float FACING_DOWN=PI*1.5f;
 
 #endif //__UtilH__
